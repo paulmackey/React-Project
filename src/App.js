923028c1d1c871c5ce71c63264080b221455e141
Paul Mackey <mackey.paul.23@gmail.com>
2018-07-08 17:09:54 +0100

    Another commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcomeee to Paul Mackeys React App</h1>
+          <h1 className="App-title">Welcomeseeaa to Paul Mackeys React App</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
