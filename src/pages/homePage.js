a7455ba3b9683fa1de29af93c7ceaece594ec0bc
Paul Mackey <mackey.paul.23@gmail.com>
2018-07-09 00:40:37 +0100

    Test

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -10,6 +10,10 @@ class HomePage extends Component {
       <h1 className="mt-3">Paul Mackey</h1>
       <p>Web Developer</p>
       <p className="">Irish Web Headquarters</p>
+      <div className="social-icons">
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/paulmackey"><i className="fa fa-github"></i></a>
+<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/paul-mackey-25543098/" ><i className="fa fa-linkedin"></i></a>
+</div>
       <p className="text-emoji">\ (•◡•) /</p>
       </div>
       </div>
