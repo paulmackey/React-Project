271c6864a8af19ded2a47ca0e506a1aa5fd28f36
Paul Mackey <mackey.paul.23@gmail.com>
2018-07-09 21:08:16 +0100

    fix scroll

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div id="app">
+      <div id="app_">
         <Router>
           <div>
             <NavBar />
